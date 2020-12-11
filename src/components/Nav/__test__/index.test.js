5debbd245febd852a23e33839d9cc520dd27e658
Mahmoud Abdulrhman <mahmoudtheeb@gmail.com>
2020-12-11 01:02:32 -0800

    add gallery

--- src/components/Nav/__test__/index.test.js
+++ src/components/Nav/__test__/index.test.js
@@ -1,41 +1,61 @@
+// __tests__/Nav.test.js with hard coded categories
 import React from 'react';
-import {render , cleanup} from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import Nav  from '..';
+import Nav from '..';
+
+
+const categories = [
+  { name: 'portraits', description: 'Portraits of people in my life' }
+]
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
 
 afterEach(cleanup);
 
-describe('Nav component', ()=>{
-    //baseline test
-    it('renders',()=>{
-        render(<Nav />);
-    })
-    //snapshot test
-    it('Match snapShot', ()=>{
-        const { asFragment } = render(<Nav />);
-        //assert value comparison
-        expect( asFragment()).toMatchSnapshot();
-     })
-})
-  
-  // test for Emoji Visibilty
 
-describe('emoji is visible', ()=>{
-    it('inserts emoji into the h2',()=>{
-        // Arrange
-        const { getByLabelText } = render(<Nav />);
-        //Asssert
-        expect(getByLabelText('camera')).toHaveTextContent('📸');
-    });
+describe('Nav component renders', () => {
+  it('renders', () => {
+    render(<Nav 
+      categories={categories}
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockCurrentCategory}
+      />);
+      
+  });
+
+  it('matches snapshot', () => {
+    const { asFragment } = render(<Nav 
+      categories={categories}
+      setCurrentCategory={mockSetCurrentCategory}
+      currentCategory={mockCurrentCategory}
+      />);
+    
+    expect(asFragment()).toMatchSnapshot();
+  });
 })
 
-// test for link Visibility
+describe('emoji is visible', () => {
+  it('inserts emoji into the h2', () => {
+  const { getByLabelText } = render(<Nav 
+    categories={categories}
+    setCurrentCategory={mockSetCurrentCategory}
+    currentCategory={mockCurrentCategory}/>);
+
+  expect(getByLabelText('camera')).toHaveTextContent('📸');
+  });
+})  
 
 describe('links are visible', () => {
-    it('inserts text into the links', () => {
-      const { getByTestId } = render(<Nav />);
-      expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
-      expect(getByTestId('about')).toHaveTextContent('About me');
-    });
-  })
-  
+  it('inserts text into the links', () => {
+    const { getByTestId } = render(<Nav
+      categories={categories}
+    setCurrentCategory={mockSetCurrentCategory}
+    currentCategory={mockCurrentCategory}
+       />);
+
+    expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+    expect(getByTestId('about')).toHaveTextContent('About me');
+  });
+
+})
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,61 +1,51 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
-
-function Nav() {
-    const categories = [
-        {
-            name: "commercial",
-            description:
-            "Photos of grocery stores, food truks, and other commircial projects"
-        },
-        {
-            name:"portraits", 
-            description:
-            "Portraits of people in my life"
-        },
-        {
-            name:"food",
-            description:
-            "Delicious delicious"
-        },
-        {
-            name:"landscape",
-            description:
-            "Fields, farmhouses, waterfalls, and the beauty of nature",
-        },
-    ]
-    function categorySelected (name){
-        console.log(`${name} clicked`)
-    }
+function Nav(props) {
+  
+  const {
+    categories = [],
+    setCurrentCategory,
+    currentCategory,
+  } = props;
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
   return (
-    <header>
-         <h2>
-    <a data-testid="link" href="/">
-      <span role="img" aria-label="camera"> 📸</span> Oh Snap!
-    </a>
-  </h2>
-  <nav>
-    <ul className="flex-row">
-      <li className="mx-2">
-        <a data-testid="about" href="#about">
-          About me
+    <header className="flex-row px-1">
+      <h2>
+        <a data-testid="link" href="/">
+          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
         </a>
-      </li>
-      <li>
-        <span>Contact</span>
-      </li>
-      {categories.map((category)=>(
-          <li
-          className="mx-1"
-          key={category.name}
-          >
-              <span onClick={()=> categorySelected(category.name)} >
-                  {category.name}
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a data-testid="about" href="#about">
+              About me
+            </a>
+          </li>
+          <li className="mx-2">
+            <span>Contact</span>
+          </li>
+          {categories.map((category) => (
+            <li
+              className={`mx-1 ${
+                currentCategory.name === category.name && 'navActive'
+                }`}
+              key={category.name}
+            >
+              <span
+                onClick={() => {
+                  setCurrentCategory(category)
+                }}
+              >
+                {capitalizeFirstLetter(category.name)}
               </span>
-           </li>
-      ))}
-    </ul>
-  </nav>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 }
